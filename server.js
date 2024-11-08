b5c35c35889d3e8cf2dbd09d394039a9336f6ae7
sureshrocky86 <sureshrocky1234@gmail.com>
2024-11-08 19:22:25 +0530

    updates

--- server.js
+++ server.js
@@ -9,9 +9,23 @@ require('dotenv').config(); // Load environment variables from .env file
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+
 // Middleware
 app.use(cors());
 app.use(express.json()); // For parsing application/json
+
+app.use((req, res, next) => { 
+  if (mongoose.connection.readyState !== 1) { 
+    return res.status(503).send('MongoDB connection not ready'); 
+  } 
+  next();
+});
+
+// MongoDB Connection
+mongoose.connect(process.env.MONGODB_URI)
+.then(() => console.log(`MongoDB connected`))
+.catch(err => console.log(err));
+
 app.use('/api', userRoutes);
 
 // app.use(express.static(path.join(__dirname, '../../gym-app/dist/gym-app')));
@@ -20,18 +34,16 @@ app.use('/api', userRoutes);
 // })
 
 
-// MongoDB Connection
-mongoose.connect(process.env.MONGODB_URI)
-.then(() => console.log(`MongoDB connected`))
-.catch(err => console.log(err));
 
 // Sample route
 app.get('/', (req, res) => {
   res.send('API is running...');
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+if (!process.env.IS_SERVERLESS) {
+  app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+  });
+}
 
 module.exports.handler = ServerlessHttp(app);
